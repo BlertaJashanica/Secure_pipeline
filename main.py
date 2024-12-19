d8dbba8676d210bd96e7bd7c9a07d28f36560c5b
Blerta Jashanica <s129635@ap.be>
2024-12-19 21:14:16 +0100

    ww in commentaar

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 def hello_world():
     return "Hello, world!"
 
-SECRET_KEY = "12345"  # Dit zou de detect-secrets scan moeten triggeren
+#SECRET_KEY = "12345"  # Dit zou de detect-secrets scan moeten triggeren
